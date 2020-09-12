2fcdef203f9c40142d5b717638affdc762fe6404
yys2023 <69963749+yys2023@users.noreply.github.com>
2020-09-12 09:25:17 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 let humidity = 0
 let temperature = 0
 I2C_LCD1602.LcdInit(32)
+I2C_LCD1602.ShowString("Temp:", 0, 0)
+I2C_LCD1602.ShowString("Humi:", 0, 1)
+basic.pause(2000)
 basic.forever(function () {
     dht11_dht22.queryData(
     DHTtype.DHT11,
@@ -11,7 +14,7 @@ basic.forever(function () {
     )
     temperature = Math.round(dht11_dht22.readData(dataType.temperature))
     humidity = Math.round(dht11_dht22.readData(dataType.humidity))
-    I2C_LCD1602.ShowNumber(temperature, 0, 0)
-    I2C_LCD1602.ShowNumber(humidity, 0, 1)
-    basic.pause(2000)
+    I2C_LCD1602.ShowNumber(temperature, 5, 0)
+    I2C_LCD1602.ShowNumber(humidity, 5, 1)
+    basic.pause(10000)
 })
